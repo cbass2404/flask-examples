355e1ade0ac7bfea772a634ecd2911a19ac6fe6c
Cory Bass <cory.bass.07@gmail.com>
2020-11-16 12:22:45 -0600

    first portion finished

--- app.py
+++ app.py
@@ -9,15 +9,5 @@ def home():
     return render_template('index.html', name="Cory")
 
 
-@app.route('/slug/<id>')
-def slug(id):
-    return render_template('slug.html', id=id)
-
-
-@app.route('/loops')
-def loops():
-    return render_template('loops.html', content=['Cory', 'Zac', 'Jayden', 'Boli', 'Evalyn'])
-
-
 if __name__ == "__main__":
     app.run(debug=True)
