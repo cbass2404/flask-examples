9444eeed44ef6a4c6e08cee18838c1bee4ca65a6
Cory Bass <cory.bass.07@gmail.com>
2020-11-16 12:04:57 -0600

    02 basic html templates finished

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, redirect, url_for, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html', name="Cory")
+
+
+@app.route('/slug/<id>')
+def slug(id):
+    return render_template('slug.html', id=id)
+
+
+@app.route('/loops')
+def loops():
+    return render_template('loops.html', content=['Cory', 'Zac', 'Jayden', 'Boli', 'Evalyn'])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
