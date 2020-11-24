9bfe39e213f406d3d032c33691a46bce73344805
Cory Bass <cory.bass.07@gmail.com>
2020-11-24 11:04:27 -0600

    finished up on sessions

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 from flask import Flask, request, render_template, redirect, session
 
 app = Flask(__name__)
+app.secret_key = 'super secret key'
 
 
 @app.route('/')
 def home():
-  return render_template('index.html')
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form.get('username')
+        return redirect('/')
+    return render_template('login.html')
 
 
 if __name__ == "__main__":
-  app.debug = True
-  app.run()
+    app.debug = True
+    app.run()
